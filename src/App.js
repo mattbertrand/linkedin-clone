4e8a9392d0ef8f545db36f82f5178204aa9cb0ae
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-02 12:13:13 -0700

    added first route to login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's do this</h1>
+      <Router>
+		  <Switch>
+			  <Route exact path="/">
+				  <Login />
+			  </Route>
+		  </Switch>
+	  </Router>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Login = (props) => {
+	return <div>Login</div>
+}
+
+export default Login;
