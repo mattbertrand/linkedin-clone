f5ddf157a8d97762d3d4d4f33762cae77e2b1c2b
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-02 12:32:16 -0700

    added login logo and styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,36 @@
 import styled from 'styled-components';
 
 const Login = (props) => {
-	return <div>Login</div>
+	return (
+		<Container>
+			<Nav>
+				<a href="/">
+					<img src="/images/login-logo.svg" alt="linkedin-login-logo"/>
+				</a>
+			</Nav>
+		</Container>
+	)
 }
+
+const Container = styled.div``;
+
+const Nav = styled.nav`
+	max-width: 1128px;
+	margin: auto;
+	padding: 12px 0 16px;
+	display: flex;
+	align-items: center;
+	position: relative;
+	justify-content: space-between;
+	flex-wrap: nowrap;
+
+	& > a {
+		width: 135px;
+		height: 34px;
+		@media (max-width: 768px) {
+			padding: 0 5px;
+		}
+	}
+`;
 
 export default Login;
