49c50d825df4a47612a16cc23f30fe4ef1b1cbff
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-02 12:43:51 -0700

    added sign in component and styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,7 @@ const Login = (props) => {
 				</a>
 				<div>
 					<Join>Join Now</Join>
+					<SignIn>Sign In</SignIn>
 				</div>
 			</Nav>
 		</Container>
@@ -46,6 +47,24 @@ const Join = styled.a`
 	&:hover {
 		background-color: rgba(0, 0, 0, 0.08);
 		color: rgba(0, 0, 0, 0.9);
+		text-decoration: none;
+	}
+`;
+
+const SignIn = styled.a`
+	box-shadow: inset 0 0 0 1px #0a66c2;
+	color: #0a66c2;
+	border-radius: 24px;
+	transition-duration: 167ms;
+	font-size: 16px;
+	font-weight: 600;
+	line-height: 40px;
+	padding: 10px 12px;
+	text-align: center;
+	background-color: rgba(0, 0, 0, 0);
+	&:hover {
+		background-color: rgba(112, 181, 249, 0.15);
+		color: #0a66c2;
 		text-decoration: none;
 	}
 `;
