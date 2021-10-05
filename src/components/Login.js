a746fccc9234583f338b55b0c84f3ee27a0daa7b
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-05 16:57:12 -0700

    added a Hero component and styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,10 @@ const Login = (props) => {
 				</div>
 			</Nav>
 			<Section>
-				section
+				<Hero>
+					<h1>Welcome to your professional community</h1>
+					<img src="/images/login-hero.svg" alt="login-hero" />
+				</Hero>
 			</Section>
 		</Container>
 	)
@@ -82,12 +85,44 @@ const Section = styled.section`
 	position: relative;
 	flex-wrap: wrap;
 	width: 100%;
-	max-width: 128px;
+	max-width: 1128px;
 	align-items: center;
 	margin: auto;
 	@media (max-width: 768px) {
 		margin: auto;
 		min-height: 0px;
+	}
+`;
+
+const Hero = styled.div`
+	width: 100%;
+	h1 {
+		padding-bottom: 0;
+		width: 55%;
+		font-size: 56px;
+		color: #2977c9;
+		font-weight: 200;
+		line-height: 70px;
+		@media (max-width: 768px) {
+			text-align: center;
+			font-size: 20px;
+			width: 100%;
+			line-height: 2;
+		}
+	}
+	img {
+		/* z-index: -1; */
+		width: 700px;
+		height: 670px;
+		position: absolute;
+		bottom: -2px;
+		right: -150px;
+		@media (max-width: 768px) {
+			top: 230px;
+			width: initial;
+			position: initial;
+			height: initial;
+		}
 	}
 `;
 
