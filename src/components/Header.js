8835658a41889ddd6cfa99b9264ac59c544ffcf3
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-05 17:21:51 -0700

    styled the container and content components and imported image home logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,36 @@
 import styled from "styled-components";
 
 const Header = (props) => {
-	return <div>Header</div>
+	return (
+		<Container>
+			<Content>
+				<Logo>
+					<a href="/home">
+						<img src="/images/home-logo.svg" alt="linkedin-home-logo" />
+					</a>
+				</Logo>
+			</Content>
+		</Container>
+		)
 };
+
+const Container = styled.div`
+	background-color: white;
+	border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+	left: 0;
+	padding: 0 24px;
+	position: fixed;
+	top: 0;
+	width: 100vw;
+	z-index: 100;
+`;
+
+const Content = styled.div`
+	display: flex;
+	align-items: center;
+	margin: 0 auto;
+	min-height: 100%;
+	max-width: 1128px;
+`;
 
 export default Header;
