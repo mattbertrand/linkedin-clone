c4adefb3363b56fc56385eab8f903bf4e96e43f1
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-05 17:41:54 -0700

    styled Search and SearchIcon Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,14 @@ const Header = (props) => {
 						<img src="/images/home-logo.svg" alt="linkedin-home-logo" />
 					</a>
 				</Logo>
+				<Search>
+					<div>
+						<input type="text" placeholder="Search" />
+					</div>
+				<SearchIcon>
+					<img src="/images/search-icon.svg" alt="search-icon" />
+				</SearchIcon>
+				</Search>
 			</Content>
 		</Container>
 		)
@@ -31,6 +39,49 @@ const Content = styled.div`
 	margin: 0 auto;
 	min-height: 100%;
 	max-width: 1128px;
+`;
+
+const Logo = styled.span`
+	margin-right: 8px;
+	font-size: 0px;
+`;
+
+const Search = styled.div`
+	opacity: 1;
+	flex-grow: 1;
+	position: relative;
+	& > div {
+		max-width: 280px;
+		input {
+			border: none;
+			box-shadow: none;
+			background-color: #eef3f8;
+			border-radius: 2px;
+			color: rgba(0, 0, 0, 0.9);
+			width: 218px;
+			padding: 0 8px 0 40px;
+			line-height: 1.75;
+			font-weight: 400;
+			font-size: 14px;
+			height: 34px;
+			border-color: #dce6f1;
+			vertical-align: text-top;
+		}
+	}
+`;
+
+const SearchIcon = styled.div`
+	width: 40px;
+	position: absolute;
+	z-index: 1;
+	top: 10px;
+	left: 2px;
+	border-radius: 0 2px 2px 0;
+	margin: 0;
+	pointer-events: 0;
+	display: flex;
+	justify-content: center;
+  	align-items: center;
 `;
 
 export default Header;
