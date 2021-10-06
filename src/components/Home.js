f350c17960972965f289b0b60166b885872fd02d
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-06 16:06:46 -0700

    added a Layout component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,11 @@ const Home = (props) => {
 				<h5><a>Hiring in a Hurry?</a></h5>
 				<p>Find talented pros in record time with Upwork and keep business moving</p>
 			</Section>
+			<Layout>
+				<div>Left side</div>
+				<div>Main</div>
+				<div>Right side</div>
+			</Layout>
 		</Container>
 		)
 };
@@ -44,6 +49,21 @@ const Section = styled.section`
 		font-weight: 600;
 	}
 	@media (max-width: 768px) {
+		flex-direction: column;
+		padding: 0 5px;
+	}
+`;
+
+const Layout = styled.div`
+	display: grid;
+	grid-template-areas: "leftside main rightside";
+	grid-template-columns: minmax()(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+	column-gap: 25px;
+	row-gap: 25px;
+	/* grid-template-rows: auto; */
+	margin: 25px 0;
+	@media (max-width: 768px) {
+		display: flex;
 		flex-direction: column;
 		padding: 0 5px;
 	}
