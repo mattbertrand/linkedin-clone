f94f8be3ecd14c1a76cc61719050d999f8333fdc
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-06 16:00:56 -0700

    added an add to hompe age

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,52 @@
 import styled from "styled-components";
 
 const Home = (props) => {
-	return <div>Home</div>
+	return (
+		<Container>
+			<Section>
+				<h5><a>Hiring in a Hurry?</a></h5>
+				<p>Find talented pros in record time with Upwork and keep business moving</p>
+			</Section>
+		</Container>
+		)
 };
+
+const Container = styled.div`
+	padding-top: 52px;
+	max-width: 100%;
+
+`;
+
+const Content = styled.div`
+	max-width: 1128px;
+	margin-left: auto;
+	margin-right: auto;
+`;
+
+const Section = styled.section`
+	min-height: 50px;
+	padding: 16px 0;
+	box-sizing: content-box;
+	text-align: center;
+	text-decoration: underline;
+	display: flex;
+	justify-content: center;
+	h5 {
+		color: #0a66c2;
+		font-size: 14px;
+		a {
+			font-weight: 700;
+		}
+	}
+	p {
+		font-size: 14px;
+		color: #434649;
+		font-weight: 600;
+	}
+	@media (max-width: 768px) {
+		flex-direction: column;
+		padding: 0 5px;
+	}
+`;
 
 export default Home;
