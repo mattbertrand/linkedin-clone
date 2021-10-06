73c6c0237509462afcdc9b99f2082a810c11e5ad
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-05 18:01:11 -0700

    added nav icons and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,14 +11,45 @@ const Header = (props) => {
 				</Logo>
 				<Search>
 					<div>
-						<input type="text" placeholder="Search" />
+						<input type="text" placeholder="Search for jobs, companies..." />
 					</div>
 				<SearchIcon>
 					<img src="/images/search-icon.svg" alt="search-icon" />
 				</SearchIcon>
 				</Search>
 				<Nav>
-					nav menu
+					<NavListWrap>
+						<NavList>
+							<a>
+								<img src="/images/nav-home.svg" alt="nav-home" />
+								<span>Home</span>
+							</a>
+						</NavList>
+						<NavList>
+							<a>
+								<img src="/images/nav-network.svg" alt="nav-home" />
+								<span>My Network</span>
+							</a>
+						</NavList>
+						<NavList>
+							<a>
+								<img src="/images/nav-jobs.svg" alt="nav-home" />
+								<span>Jobs</span>
+							</a>
+						</NavList>
+						<NavList>
+							<a>
+								<img src="/images/nav-messaging.svg" alt="nav-home" />
+								<span>Messaging</span>
+							</a>
+						</NavList>
+						<NavList>
+							<a>
+								<img src="/images/nav-notifications.svg" alt="nav-home" />
+								<span>Notifications</span>
+							</a>
+						</NavList>
+					</NavListWrap>
 				</Nav>
 			</Content>
 		</Container>
@@ -99,5 +130,58 @@ const Nav = styled.nav`
 	}
 `;
 
+const NavListWrap = styled.ul`
+	display: flex;
+	flex-wrap: nowrap;
+	list-style-type: none;
+	.active {
+		span:after {
+			content: "";
+			transform: scaleX(1);
+			border-bottom: 2px solid var(--white, #fff);
+			bottom: 0;
+			left: 0;
+			position: absolute;
+			transition: transform 0.2s ease-in-out;
+			width: 100%;
+			border-color: rgba(0, 0, 0, 0.9);
+    }
+  }
+`;
+
+const NavList = styled.li`
+	display: flex;
+	align-items: center;
+	a {
+		align-items: center;
+		background: transparent;
+		display: flex;
+		flex-direction: column;
+		font-size: 12px;
+		font-weight: 400;
+		justify-content: center;
+		line-height: 1.5;
+		min-height: 52px;
+		min-width: 80px;
+		position: relative;
+		text-decoration: none;
+	span {
+		color: rgba(0, 0, 0, 0.6);
+		display: flex;
+		align-items: center;
+		}
+		@media (max-width: 768px) {
+		min-width: 70px;
+		}
+	}
+	&:hover,
+	&:active {
+		a {
+			span {
+				color: rgba(0, 0, 0, 0.9);
+      }
+    }
+  }
+`;
 
 export default Header;
