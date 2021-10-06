d4eecad901629de5550b27bae5323cc503392a04
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-05 19:34:16 -0700

    added icons to navbar and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = (props) => {
 				</Search>
 				<Nav>
 					<NavListWrap>
-						<NavList>
+						<NavList className="active">
 							<a>
 								<img src="/images/nav-home.svg" alt="nav-home" />
 								<span>Home</span>
@@ -49,6 +49,27 @@ const Header = (props) => {
 								<span>Notifications</span>
 							</a>
 						</NavList>
+						<User>
+							<a>
+								<img src="images/user.svg" alt="user" />
+								<span>Me</span>
+								<img src="/images/down-icon.svg" alt="down-icon" />
+							</a>
+						</User>
+						<Work>
+							<a>
+								<img src="/images/nav-work.svg" alt="" />
+								<span>Work
+									<img src="/images/down-icon.svg" alt="down-icon" />
+								</span>
+							</a>
+						</Work>
+						<PostAJob>
+							<a>
+								<img src="/images/post-a-job.svg" alt="post-a-job" />
+								<span>Post a Job</span>
+							</a>
+						</PostAJob>
 					</NavListWrap>
 				</Nav>
 			</Content>
@@ -182,6 +203,31 @@ const NavList = styled.li`
       }
     }
   }
+`;
+
+const User = styled(NavList)`
+	a > svg {
+		width: 24px;
+		border-radius: 50%;
+	}
+
+	a > img {
+		width: 24px;
+		height: 24px;
+		border-radius: 50%;
+	}
+
+	span {
+		display: flex;
+		align-items: center;
+	}
+`;
+
+const Work = styled(User)`
+	border-left: 1px solid rgba(0, 0, 0, 0.08);
+`;
+
+const PostAJob = styled(User)`
 `;
 
 export default Header;
