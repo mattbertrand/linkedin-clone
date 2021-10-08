17913db5e493e4c9d6bab6e19efdf8324d3aeb93
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-08 00:28:05 -0700

    added an action file

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import { auth, provider } from "../firebase";
+
+export function signInAPI() {
+	return (dispatch) => {
+		auth.signInWithPopup(provider).then((payload) => {
+			console.log(payload)
+		})
+		.catch((error) => alert(error.message))
+	}
+}
