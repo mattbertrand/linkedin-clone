edec6673060537cc0fa9b9262cc1c146e270c48e
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-08 00:10:47 -0700

    added a store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,3 +4,5 @@ import userReducer from './userReducer';
 const rootReducer = combineReducers({
 	userState: userReducer,
 })
+
+export default rootReducer;
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -8,3 +8,5 @@ const userReducer = (state = INITIAL_STATE, action) => {
 			return state
 	}
 }
+
+export default userReducer;
