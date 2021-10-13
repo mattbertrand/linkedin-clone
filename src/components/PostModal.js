a2a29a73d7bb67f01e9b3de711a16838ea5a3390
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-12 18:42:27 -0700

    added a Editor component with textarea

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
+import { useState } from 'react';
 
 const PostModal = (props) => {
+	const [editorText, setEditorText] = useState('');
+	
 	return (
 		<Container>
 			<Content>
@@ -15,6 +18,15 @@ const PostModal = (props) => {
 						<img src="/images/user.svg" alt="user"/>
 						<span>Name</span>
 					</UserInfo>
+					<Editor>
+						<textarea 
+							value={editorText} 
+							onChange={(e) => setEditorText(e.target.value)}
+							placeholder="What do you want to talk about?"
+							autoFocus={true}
+						>
+						</textarea>
+					</Editor>
 				</SharedContent>
 				<ShareCreation>
 					<AttachAssets>
@@ -157,6 +169,21 @@ const PostButton = styled.button`
 	color: white;
 	&:hover {
 		background: #004182;
+	}
+`;
+
+const Editor = styled.div`
+	padding: 12px 24px;
+	textarea {
+		width: 100%;
+		min-height: 100px;
+		resize: none;
+	}
+	input {
+		width: 100%;
+		height: 35px;
+		font-size: 16px;
+		margin-bottom: 20px;
 	}
 `;
 
