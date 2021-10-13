f120b606ff1042d4c9a4bf1ced9953e92ee09d9e
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-10-12 19:51:09 -0700

    added an updloadImage component to upldao images

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -3,6 +3,17 @@ import { useState } from 'react';
 
 const PostModal = (props) => {
 	const [editorText, setEditorText] = useState('');
+	const [shareImage, setShareImage] = useState('');
+
+	const handleChange = (e) => {
+		const image = e.target.files[0];
+
+		if (image === "" || image === undefined) {
+			alert(`not an image, the file is a ${typeof image}`)
+			return;
+		}
+		setShareImage(image);
+	}
 	
 	const reset = (e) => {
 		setEditorText("")
@@ -31,8 +42,21 @@ const PostModal = (props) => {
 									onChange={(e) => setEditorText(e.target.value)}
 									placeholder="What do you want to talk about?"
 									autoFocus={true}
-								>
-								</textarea>
+								/>
+								<UploadImage>
+									<input 
+									type="file" 
+									accept="gif, jpeg, png"
+									name="image"
+									id="file"
+									style={ {display: "none" } }
+									onChange={handleChange}
+									/>
+									<p>
+										<label htmlFor="file">Select an image to share</label>
+									</p>
+									{shareImage && <img src={URL.createObjectURL(shareImage)} />}
+								</UploadImage>
 							</Editor>
 						</SharedContent>
 						<ShareCreation>
@@ -209,6 +233,13 @@ const Editor = styled.div`
 		height: 35px;
 		font-size: 16px;
 		margin-bottom: 20px;
+	}
+`;
+
+const UploadImage = styled.div`
+	text-align: center;
+	img {
+		width: 100%;
 	}
 `;
 
